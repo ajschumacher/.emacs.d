c56723ae43b7e47c85d4dffe4a6a3ffe992fe5a8
Aaron Schumacher <ajschumacher@gmail.com>
2014-10-03 21:40:59 -0400

    drops all matplotlib stuff; closes #19

--- link.sh
+++ link.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir ~/.matplotlib/
-ln -s ~/newdot/.matplotlib/matplotlibrc ~/.matplotlib/matplotlibrc
 ln -s ~/newdot/.emacs ~/.emacs
 ln -s ~/newdot/.emacs.d/ ~/.emacs.d
 ln -s ~/newdot/.gitconfig ~/.gitconfig
