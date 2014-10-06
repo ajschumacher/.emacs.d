2ceb5cc78f0d30e50a430b0798b011757a829ea5
Aaron Schumacher <ajschumacher@gmail.com>
2014-10-05 23:46:00 -0400

    specify jedi

--- init.el
+++ init.el
@@ -237,6 +237,7 @@
 
 ;; this is messed with by emacs if you let it...
 (custom-set-variables
+ '(elpy-rpc-backend "jedi")
  '(help-at-pt-display-when-idle (quote (flymake-overlay)) nil (help-at-pt))
  '(help-at-pt-timer-delay 0.9)
  '(tab-width 4))
@@ -344,3 +345,9 @@ file of a buffer in an external program."
       smtpmail-smtp-service 587
       user-full-name "Aaron Schumacher"
       user-mail-address "ajschumacher@gmail.com")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
