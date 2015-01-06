3238b9c6f7ecb86e16ef31ec00f112d6bb0343b5
Aaron Schumacher <ajschumacher@gmail.com>
2015-01-06 13:58:03 +0000

    inhibit-startup-screen (closes #29)

--- init.el
+++ init.el
@@ -217,6 +217,7 @@
 ;;; UI things for display
 
 ;; get rid of UI stuff (not sure all of these always apply)
+(setq inhibit-startup-screen t)
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
