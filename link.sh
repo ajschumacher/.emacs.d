bdc572da8355c19bb2145355ea481df078c0128a
Aaron Schumacher <ajschumacher@gmail.com>
2015-09-21 11:16:37 -0400

    reconsider virtualenv setup...

--- link.sh
+++ link.sh
@@ -6,5 +6,3 @@ ln -s ~/.bashrc ~/.bash_profile
 ln -s ~/.emacs.d/bash/.git-completion.sh ~/.git-completion.sh
 ln -s ~/.emacs.d/bash/.git-prompt.sh ~/.git-prompt.sh
 ln -s ~/.emacs.d/ruby/.irbrc ~/.irbrc
-
-mkdir -p ~/.virtualenvs
