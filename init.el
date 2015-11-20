58aec6ecbc7f9cf66868f153efec90a0ee79cad9
Aaron Schumacher <ajschumacher@gmail.com>
2015-11-20 18:00:37 -0500

    check

--- init.el
+++ init.el
@@ -201,7 +201,7 @@
              (expand-file-name "git-gutter-plus" user-emacs-directory))
 (require 'git-gutter+)
 (global-git-gutter+-mode)
-; change! again! again! and again!
+; change! clear!
 ;; Show where files have changed vs. last commit.
 ;; (use-package git-gutter-fringe+
 ;;   :ensure nil
