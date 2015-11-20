3bf9e87115c7668b38f8e9c591c7b6fbb5bf4f5e
Aaron Schumacher <ajschumacher@gmail.com>
2015-11-20 15:35:51 -0500

    Cask removed

--- init.el
+++ init.el
@@ -38,22 +38,19 @@
 
 ;;; Set some defaults.
 
-;; TODO: reconsider
-(setq-default major-mode 'text-mode)
-
 ;; Recommended by Mickey.
 (setq apropos-sort-by-scores t)
 
 ;; ThisIsFourWords
 (global-subword-mode t)
 
-;; shift-arrows for changing windows
+;; Use shift-arrows for changing windows.
 (windmove-default-keybindings)
 
-;; and I definitely want to see where buffers end
+;; Show where buffers end.
 (setq-default indicate-empty-lines t)
 
-;; I guess I have to turn this on...
+;; Consider using abbreviations.
 (abbrev-mode)
 
 ;; Be aware of whitespace.
@@ -61,7 +58,7 @@
 (global-whitespace-mode)
 (diminish 'global-whitespace-mode)
 
-;; Don't insert tabs!
+;; Don't insert tabs.
 (setq-default indent-tabs-mode nil)
 
 ;; Use just 'y' or 'n', not 'yes' or 'no'.
@@ -96,7 +93,7 @@
 ;; Update the screen by one line, not one page.
 (setq scroll-step 1)
 
-;; Allow region downcase w/ C-x C-l, upcase w/ C-x C-u
+;; Allow region downcase w/ C-x C-l, upcase w/ C-x C-u.
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
@@ -115,7 +112,7 @@
                             (flyspell-prog-mode)
                             (diminish 'flyspell-mode)))
 
-;; Show off lambdas everywhere
+;; Show off lambdas everywhere.
 (when (and (<= 24 emacs-major-version)
            (<= 4 emacs-minor-version))
   (define-globalized-minor-mode
@@ -184,13 +181,13 @@
   :diminish whole-line-or-region-mode)
 
 
-;; marks for changes
+;; Show where files have changed vs. last commit.
 (use-package git-gutter-fringe+
   :config (global-git-gutter+-mode t)
   :diminish git-gutter+-mode)
 
 
-;; Interactive
+;; Interactive selection of things.
 ;; TODO: consider helm instead (see Sacha's config)
 (ido-mode t)
 (ido-everywhere t)
@@ -219,7 +216,7 @@
   (global-set-key (kbd "M-X") 'smex-major-mode-commands))
 
 
-;; projectile adds nice project functions for git repos.
+;; Add nice project functions for git repos.
 (use-package projectile
   :config (projectile-global-mode)
   :diminish projectile-mode)
@@ -255,8 +252,9 @@
 
 ;; Edit in multiple places at the same time.
 (use-package multiple-cursors
-  :bind (("C-x r t" . 'mc/edit-lines)
-         ("C-x C-x" . 'mc/mark-more-like-this-extended)))
+  :bind
+  ("C-x r t" . mc/edit-lines)
+  ("C-x C-x" . mc/mark-more-like-this-extended))
 
 
 ;; (Near) simultaneous keypresses create new keys.
@@ -280,7 +278,7 @@
 
 ;; expand-region is that new hotness.
 (use-package expand-region
-  :config (global-set-key (kbd "M-o") 'er/expand-region))
+  :bind ("M-o" . er/expand-region))
 
 
 ;; Work well with parentheses and friends.
@@ -294,9 +292,10 @@
 
 ;; Conveniently zoom all of Emacs.
 (use-package zoom-frm
-  :bind (("C-=" . zoom-in/out)
-         ("C-+" . zoom-in/out)
-         ("C--" . zoom-in/out)))
+  :bind
+  ("C-=" . zoom-in/out)
+  ("C-+" . zoom-in/out)
+  ("C--" . zoom-in/out))
 
 
 ;; Search the web from Emacs.
@@ -386,3 +385,138 @@
     (diminish 'rainbow-mode))
   (add-hook 'html-mode-hook 'rainbow-mode-quietly)
   (add-hook 'css-mode-hook 'rainbow-mode-quietly))
+
+
+;;; Functions Written by others:
+
+(defun prelude-open-with ()
+  "Simple function that allows us to open the underlying
+file of a buffer in an external program."
+  (interactive)
+  (when buffer-file-name
+    (shell-command (concat
+                    (if (eq system-type 'darwin)
+                        "open"
+                      (read-shell-command "Open current file with: "))
+                    " "
+                    buffer-file-name))))
+(global-set-key (kbd "C-c o") 'prelude-open-with)
+
+(defun randomize-region (beg end)
+  (interactive "r")
+  (if (> beg end)
+      (let (mid) (setq mid end end beg beg mid)))
+  (save-excursion
+    ;; put beg at the start of a line and end and the end of one --
+    ;; the largest possible region which fits this criteria
+    (goto-char beg)
+    (or (bolp) (forward-line 1))
+    (setq beg (point))
+    (goto-char end)
+    ;; the test for bolp is for those times when end is on an empty
+    ;; line; it is probably not the case that the line should be
+    ;; included in the reversal; it isn't difficult to add it
+    ;; afterward.
+    (or (and (eolp) (not (bolp)))
+        (progn (forward-line -1) (end-of-line)))
+    (setq end (point-marker))
+    (let ((strs (shuffle-list
+                 (split-string (buffer-substring-no-properties beg end)
+                             "\n"))))
+      (delete-region beg end)
+      (dolist (str strs)
+        (insert (concat str "\n"))))))
+
+(defun shuffle-list (list)
+  "Randomly permute the elements of LIST.
+All permutations equally likely."
+  (let ((i 0)
+  j
+  temp
+  (len (length list)))
+    (while (< i len)
+      (setq j (+ i (random (- len i))))
+      (setq temp (nth i list))
+      (setcar (nthcdr i list) (nth j list))
+      (setcar (nthcdr j list) temp)
+      (setq i (1+ i))))
+  list)
+
+;; the-the in honor of An Introduction to Programming in Emacs Lisp
+(defun the-the ()
+  "Search forward for for a duplicated word."
+  (interactive)
+  (message "Searching for for duplicated words ...")
+  (push-mark)
+  ;; This regexp is not perfect
+  ;; but is fairly good over all:
+  (if (re-search-forward
+       "\\b\\([^@ \n\t]+\\)[ \n\t]+\\1\\b" nil 'move)
+      (message "Found duplicated word.")
+    (message "End of buffer")))
+
+
+;;; Functions written by me:
+
+(defun space-tab (current desired)
+  "Change size of space tabs."
+  (interactive "nCurrent size: \nnDesired size: ")
+    (setq tab-width current)
+    (tabify (point-min) (point-max))
+    (setq tab-width desired)
+    (untabify (point-min) (point-max))
+    (setq tab-width desired)
+    (setq python-indent desired))
+
+(defun ajs-decimal-escapes-to-unicode ()
+  "Convert escapes like '&#955;' to Unicode like 'λ'.
+Operates on the active region or the whole buffer."
+  (interactive)
+  (let ((start (point)) (end (mark)))
+    (or (use-region-p)
+        (setq start (point-min) end (point-max)))
+    (insert (replace-regexp-in-string
+             "&#[0-9]*;"
+             (lambda (match)
+               (format "%c" (string-to-number (substring match 2 -1))))
+             (filter-buffer-substring start end t)))))
+
+(defun ajs-run-in-file-and-save (filename function)
+  "Run the function in a buffer for the FILE and save it"
+  (save-excursion
+    (let ((buffer (find-file-noselect filename)))
+      (message "Working on %s" filename)
+      (set-buffer buffer)
+      (funcall function)
+      (save-buffer)
+      (kill-buffer buffer))))
+
+(defun ajs-run-in-many-files-and-save (list-of-filenames function)
+  (dolist (filename list-of-filenames)
+    (ajs-run-in-file-and-save filename function)))
+
+(defun ajs-md-html-files-in-below-directory (directory)
+  "List the .md and .html files in DIRECTORY and in its sub-directories."
+  (interactive "DDirectory name: ")
+  (let (all-files-list
+        (current-directory-list
+         (directory-files-and-attributes directory t)))
+    (while current-directory-list
+      (cond
+       ((or
+         (equal ".md" (substring (car (car current-directory-list)) -3))
+         (equal ".html" (substring (car (car current-directory-list)) -5)))
+        (setq all-files-list
+              (cons (car (car current-directory-list)) all-files-list)))
+       ((eq t (car (cdr (car current-directory-list))))
+        (if
+            (equal "."
+                   (substring (car (car current-directory-list)) -1))
+            ()
+          (setq all-files-list
+                (append
+                 (ajs-md-html-files-in-below-directory
+                  (car (car current-directory-list)))
+                 all-files-list)))))
+      (setq current-directory-list (cdr current-directory-list)))
+    all-files-list))
