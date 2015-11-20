827177cf27793e967411cfd7c502cdde12ad88b8
Aaron Schumacher <ajschumacher@gmail.com>
2015-11-20 18:06:56 -0500

    womp

--- init.el
+++ init.el
@@ -201,7 +201,7 @@
              (expand-file-name "git-gutter-plus" user-emacs-directory))
 (require 'git-gutter+)
 (global-git-gutter+-mode)
-; change! back!
+; change! back! to!
 ;; Show where files have changed vs. last commit.
 ;; (use-package git-gutter-fringe+
 ;;   :ensure nil
