10be46e273732fbcc325c642db32d2f7ed3ec06f
Aaron Schumacher <ajschumacher@gmail.com>
2015-11-20 18:11:47 -0500

    aherm?

--- init.el
+++ init.el
@@ -201,7 +201,7 @@
              (expand-file-name "git-gutter-plus" user-emacs-directory))
 (require 'git-gutter+)
 (global-git-gutter+-mode)
-; change! back! to! something!
+; change! back! to! something! wait!
 ;; Show where files have changed vs. last commit.
 ;; (use-package git-gutter-fringe+
 ;;   :ensure nil
