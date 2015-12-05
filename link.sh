97bcb6a647b344ade56aca865e1f7b8c700d9282
Aaron Schumacher <ajschumacher@gmail.com>
2015-12-05 13:33:27 -0500

    wooooo vim? highlight trailing whitespace!

--- link.sh
+++ link.sh
@@ -7,3 +7,4 @@ ln -s ~/.bashrc ~/.bash_profile
 ln -s ~/.emacs.d/bash/.git-completion.sh ~/.git-completion.sh
 ln -s ~/.emacs.d/bash/.git-prompt.sh ~/.git-prompt.sh
 ln -s ~/.emacs.d/ruby/.irbrc ~/.irbrc
+ln -s ~/.emacs.d/vim/.vimrc ~/.vimrc
---
+++ vim/.vimrc
@@ -0,0 +1,1 @@
+match ErrorMsg '\s\+$'
