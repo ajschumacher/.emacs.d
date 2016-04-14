eaf48e79ea829be75f2b641816f8a0aef41b3008
Aaron Schumacher <ajschumacher@gmail.com>
2016-04-14 11:01:30 -0400

    make things look nice in ipython

--- link.sh
+++ link.sh
@@ -8,3 +8,5 @@ ln -s ~/.emacs.d/bash/.git-completion.sh ~/.git-completion.sh
 ln -s ~/.emacs.d/bash/.git-prompt.sh ~/.git-prompt.sh
 ln -s ~/.emacs.d/ruby/.irbrc ~/.irbrc
 ln -s ~/.emacs.d/vim/.vimrc ~/.vimrc
+mkdir -p ~/.ipython/profile_default
+ln -s ~/.emacs.d/ipython/ipython_config.py ~/.ipython/profile_default/ipython_config.py
